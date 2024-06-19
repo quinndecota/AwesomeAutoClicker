12f51af95d25b62be2b3f22f94c3cfbdbc265bd0
qtdbros3 <qtdbros3@outlook.com>
2024-06-19 17:06:22 -0500

    Files?

---
+++ Models/Action.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeAutoClicker.Models
+{
+    public class Action
+    {
+        public Action(string type, string? clicktype , int? xpos, int? ypos , int? milliseconds , string? message , string? cmdChar )
+        {
+            Type = type;
+            ClickType = clicktype;
+            Xpos = xpos;
+            Ypos = ypos;
+            Milliseconds = milliseconds;
+            Message = message;
+            CmdChar = cmdChar;
+        }
+
+        public string? Type { get; set; }
+        public string? ClickType { get; set; }
+        public int? Xpos { get; set; }
+        public int? Ypos { get; set; }
+        public int? Milliseconds { get; set; }
+        public string? Message { get; set; }
+        public string? CmdChar { get; set; }
+    }
+}
---
+++ Models/GridViewAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeAutoClicker.Models
+{
+    public class GridViewAction
+    {
+        public string Type;
+        public string Description;
+
+        public GridViewAction(string type, string description)
+        {
+            Type = type;
+            Description = description;
+        }
+    }
+}
