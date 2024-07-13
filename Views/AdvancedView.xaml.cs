4a900cbc0f428faafbc4b328e83ff88e531cf681
qtdbros3 <qtdbros3@outlook.com>
2024-07-12 21:50:06 -0500

    Many Features added and bugs removed

--- Views/AdvancedView.xaml.cs
+++ Views/AdvancedView.xaml.cs
@@ -35,6 +35,10 @@ namespace AwesomeAutoClicker.Views
         public ICommand showExportCommand { get; set; }
         ObservableCollection<string> gridViewActions = new ObservableCollection<string>();
 
+        double screenWidth = SystemParameters.PrimaryScreenWidth;
+        double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+
         public AdvancedView()
         {
             InitializeComponent();
@@ -86,42 +90,45 @@ namespace AwesomeAutoClicker.Views
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
+            var index = actions.SelectedIndex;
+
+            if(index == -1)
+            {
+                index = script.Count ;
+            }
 
             if (TypeComboBox.Text == "Click")
             {
                 if (XPos.Text == null || XPos.Text.Length == 0 || YPos.Text == null || YPos.Text.Length == 0 || ClickType.Text == null || ClickType.Text.Length == 0)
                 { return; }
                 Models.Action currentAction = new Models.Action(TypeComboBox.Text, ClickType.Text, int.Parse(XPos.Text), int.Parse(YPos.Text), null, null, null);
-                gridViewActions.Add(currentAction.ClickType + " click at (" + currentAction.Xpos + "," + currentAction.Ypos + ")");
-                script.Add(currentAction);
+                gridViewActions.Insert(index, currentAction.ClickType + " click at (" + currentAction.Xpos + "," + currentAction.Ypos + ")");
+                script.Insert(index, currentAction);
             }
             else if (TypeComboBox.Text == "Command")
             {
                 if (CmdChar.Text == null || CmdChar.Text.Length == 0)
                 { return; }
                 Models.Action currentAction = new Models.Action(TypeComboBox.Text, null, null, null, null, null, CmdChar.Text);
-                gridViewActions.Add("Ctrl + " + currentAction.CmdChar);
-                script.Add(currentAction);
+                gridViewActions.Insert(index, "Ctrl + " + currentAction.CmdChar);
+                script.Insert(index, currentAction);
             }
             else if (TypeComboBox.Text == "Interval")
             {
                 if (Milliseconds.Text == null || Milliseconds.Text.Length == 0)
                 { return; }
                 Models.Action currentAction = new Models.Action(TypeComboBox.Text, null, null, null, int.Parse(Milliseconds.Text), null, null);
-                gridViewActions.Add("Wait: " + currentAction.Milliseconds + " milliseconds");
-                script.Add(currentAction);
+                gridViewActions.Insert(index, "Wait: " + currentAction.Milliseconds + " milliseconds");
+                script.Insert(index, currentAction);
             }
             else if (TypeComboBox.Text == "Send")
             {
                 if (Message.Text == null || Message.Text.Length == 0)
                 { return; }
                 Models.Action currentAction = new Models.Action(TypeComboBox.Text, null, null, null, null, Message.Text, null);
-                gridViewActions.Add("Send Message: " + currentAction.Message);
-                script.Add(currentAction);
-
-
+                gridViewActions.Insert(index, "Send Message: " + currentAction.Message);
+                script.Insert(index, currentAction);
             }
-
         }
 
         private void Del_Click(object sender, RoutedEventArgs e)
@@ -178,8 +185,10 @@ namespace AwesomeAutoClicker.Views
 
         private void CommandActionCommand(string? cmdChar)
         {
-            input.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C);
+            input.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, GetVirtualKeyCode(cmdChar));
         }
+
+        
 
         private void SendActionCommand(string? message)
         {
@@ -193,15 +202,19 @@ namespace AwesomeAutoClicker.Views
 
         private void ClickActionCommand(string clickType, int xpos, int ypos)
         {
-            input.Mouse.MoveMouseTo((xpos / 19.2) * 500, (ypos / 10.8) * 500);
+            input.Mouse.MoveMouseTo((xpos* 65550 / (screenWidth)) , (ypos * 65550 / (screenHeight)) );
 
             if (clickType == "Left")
             {
                 input.Mouse.LeftButtonClick();
             }
-            else
+            else if(clickType == "Right")
             {
                 input.Mouse.RightButtonClick();
+            }
+            else if (clickType == "Hover")
+            {
+                return;
             }
 
         }
@@ -225,6 +238,10 @@ namespace AwesomeAutoClicker.Views
                     if (currentAction.ClickType == "Right")
                     {
                         scriptString += "2";
+                    }
+                    if (currentAction.ClickType == "Hover")
+                    {
+                        scriptString += "3";
                     }
                     scriptString += currentAction.Xpos.ToString() + "," + currentAction.Ypos.ToString();
                 }
@@ -256,47 +273,170 @@ namespace AwesomeAutoClicker.Views
             gridViewActions.Clear();
             script.Clear();
 
-            string scriptString = ImportTextbox.Text;
-
-            string[] parts = scriptString.Split(new char[] { '!', '@', '#', '$' });
-            for (int i = 1; i < parts.Length; i++) // Start from 1 to skip the initial '0'
+             string scriptString = ImportTextbox.Text;
+           
+            try
             {
-                char prefix = scriptString[scriptString.IndexOf(parts[i]) - 1];
-                Models.Action action = new(null, null, null, null, null, null, null);
-
-                switch (prefix)
+                string[] parts = scriptString.Split(new char[] { '!', '@', '#', '$' });
+                if (parts.Length <2)
                 {
-                    case '!':
-                        action.Type = "Click";
-                        action.ClickType = parts[i][0] == '1' ? "Left" : "Right";
-                        string[] coordinates = parts[i].Substring(1).Split(',');
-                        action.Xpos = int.Parse(coordinates[0]);
-                        action.Ypos = int.Parse(coordinates[1]);
-                        gridViewActions.Add(action.ClickType + " click at (" + action.Xpos + "," + action.Ypos + ")");
-                        break;
-                    case '@':
-                        action.Type = "Interval";
-                        action.Milliseconds = int.Parse(parts[i]);
-                        gridViewActions.Add("Wait: " + action.Milliseconds + " milliseconds");
-                        break;
-                    case '#':
-                        action.Type = "Command";
-                        action.CmdChar = parts[i];
-                        gridViewActions.Add("Ctrl + " + action.CmdChar);
-                        break;
-                    case '$':
-                        action.Type = "Send";
-                        action.Message = parts[i];
-                        gridViewActions.Add("Send Message: " + action.Message);
-                        break;
+                    throw new Exception();
                 }
-                script.Add(action);
+                for (int i = 1; i < parts.Length; i++) // Start from 1 to skip the initial '0'
+                {
+                    char prefix = scriptString[scriptString.IndexOf(parts[i]) - 1];
+                    Models.Action action = new(null, null, null, null, null, null, null);
+
+                    switch (prefix)
+                    {
+                        case '!':
+                            action.Type = "Click";
+
+                            switch (parts[i][0])
+                            {
+                                case '1':
+                                    action.ClickType = "Left";
+                                    break;
+                                case '2':
+                                    action.ClickType = "Right";
+                                    break;
+                                case '3':
+                                    action.ClickType = "Hover";
+                                    break;
+
+                            }
+
+
+                            string[] coordinates = parts[i].Substring(1).Split(',');
+                            action.Xpos = int.Parse(coordinates[0]);
+                            action.Ypos = int.Parse(coordinates[1]);
+                            gridViewActions.Add(action.ClickType + " click at (" + action.Xpos + "," + action.Ypos + ")");
+                            break;
+                        case '@':
+                            action.Type = "Interval";
+                            action.Milliseconds = int.Parse(parts[i]);
+                            gridViewActions.Add("Wait: " + action.Milliseconds + " milliseconds");
+                            break;
+                        case '#':
+                            action.Type = "Command";
+                            action.CmdChar = parts[i];
+                            gridViewActions.Add("Ctrl + " + action.CmdChar);
+                            break;
+                        case '$':
+                            action.Type = "Send";
+                            action.Message = parts[i];
+                            gridViewActions.Add("Send Message: " + action.Message);
+                            break;
+                        default:
+                            Clr_Click(sender, e);
+                            ImportErrorText.Visibility = Visibility.Visible;
+                            return;
+                    }
+                    script.Add(action);
+                }
+
+                
+
             }
+            catch
+            {
+                Clr_Click(sender, e);
 
-
-            #endregion
-
+                ImportErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+ImportErrorText.Visibility = Visibility.Hidden;
 
         }
+        #endregion
+
+
+        private IEnumerable<VirtualKeyCode> GetVirtualKeyCode(string? cmdChar)
+        {
+            switch (cmdChar.ToLower())
+            {
+                case "a":
+                    yield return VirtualKeyCode.VK_A;
+                    break;
+                case "b":
+                    yield return VirtualKeyCode.VK_B;
+                    break;
+                case "c":
+                    yield return VirtualKeyCode.VK_C;
+                    break;
+                case "d":
+                    yield return VirtualKeyCode.VK_D;
+                    break;
+                case "e":
+                    yield return VirtualKeyCode.VK_E;
+                    break;
+                case "f":
+                    yield return VirtualKeyCode.VK_F;
+                    break;
+                case "g":
+                    yield return VirtualKeyCode.VK_G;
+                    break;
+                case "h":
+                    yield return VirtualKeyCode.VK_H;
+                    break;
+                case "i":
+                    yield return VirtualKeyCode.VK_I;
+                    break;
+                case "j":
+                    yield return VirtualKeyCode.VK_J;
+                    break;
+                case "k":
+                    yield return VirtualKeyCode.VK_K;
+                    break;
+                case "l":
+                    yield return VirtualKeyCode.VK_L;
+                    break;
+                case "m":
+                    yield return VirtualKeyCode.VK_M;
+                    break;
+                case "n":
+                    yield return VirtualKeyCode.VK_N;
+                    break;
+                case "o":
+                    yield return VirtualKeyCode.VK_O;
+                    break;
+                case "p":
+                    yield return VirtualKeyCode.VK_P;
+                    break;
+                case "q":
+                    yield return VirtualKeyCode.VK_Q;
+                    break;
+                case "r":
+                    yield return VirtualKeyCode.VK_R;
+                    break;
+                case "s":
+                    yield return VirtualKeyCode.VK_S;
+                    break;
+                case "t":
+                    yield return VirtualKeyCode.VK_T;
+                    break;
+                case "u":
+                    yield return VirtualKeyCode.VK_U;
+                    break;
+                case "v":
+                    yield return VirtualKeyCode.VK_V;
+                    break;
+                case "w":
+                    yield return VirtualKeyCode.VK_W;
+                    break;
+                case "x":
+                    yield return VirtualKeyCode.VK_X;
+                    break;
+                case "y":
+                    yield return VirtualKeyCode.VK_Y;
+                    break;
+                case "z":
+                    yield return VirtualKeyCode.VK_Z;
+                    break;
+
+
+            }
+        }
+
     }
 }
